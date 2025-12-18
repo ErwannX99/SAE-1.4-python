9bea19c7138ff53de6482c1b5f97bb008a5e6467
Thenvd1134 <141870201+Thenvd1134@users.noreply.github.com>
2025-12-18 21:48:08 +0100

    Docstring faites

--- minmax.py
+++ minmax.py
@@ -2,25 +2,22 @@ from pyniryo import *
 import time
 import random
 import serial
-from logique_jeu import drop_piece, verif_gagnant, coups_valides
+from logique_jeu import drop_piece, verif_gagnant, coups_valides  #impportations des différentes parties du programme
 #importation des bibliothèques nécessaire au fonctionnement 
 depth_MINMAX = 7 #nombre de coup calculer à l'avance
 
-"""
-fonction d'évalusation: évalue et note l'état des coups jouée jusque maintenant.
-Cette note prendra la variable de "score" et sera initialisé à 0/
----------------
-Une identification de l'adversaire est effectué pour connaître le joueur et son adversaire 
-(si le joueur == 1, l'adversaire = 2 et inversement. La personne considéré comme le joueur est le minimax)
----------------
-Des fenêtres sont crées pour évaluer les coups pouvant être effectué dans les différents axe 
-(+10 si le joueur prend une position menançante, -10 si le joueur est menacée 
-et +100 si le joueur peut aligner 4 jetons (victoire))
----------------
-Le score retourné permettra d'influencer la décision du minimax au cours du jeu 
-"""
 
 def eval_fonction(matrice, joueur): 
+    """ 
+    Evalue la qualité de l'état du plateau pour le joueur
+    
+    Variable:
+        matrice : (list): Grille 2D représentant le jeu puissance 4 sous forme de matrice
+        joueur : (int): Identifiant du joueur  
+
+    Returns:
+        Score: (int) : Score calculées 
+    """
     score = 0 
     adversaire = 2 if joueur == 1 else 1
 
@@ -49,25 +46,26 @@ def eval_fonction(matrice, joueur):
 
     return score
 
-"""
-terminal = vérification si la partie est terminé ou non.
-------------------
-En cas de fin de partie, le minimax peut bénéficier d'une très grande récompense ou prendre
-une très grande pénalité avec l'addition ou la soustraction de sa profondeur le poussant à gagner 
-le plus vite possible ou a perdre le plus lentement possible 
------------------
-Simulation des coups en plaçant virtuellement un jeton et s'appelle d'elle même tout le long de 
-de sa simulation puis retire le jeton pour tester la colonne suivante
-------------------
-Le robot cherche à trouver le score le plus élevé tout en donnant à l'humain le score le plus bas
-------------------
-Un élagage alpha-bêta est effectué pour ne pas laisser l'algorithme chercher des coups perdant 
-optimisant ainsi le temps 
-
-
-"""
 
 def minmax_alpha_beta(matrice, depth, alpha, beta, maximizing, joueur): #fonctionnement du minimax & opitimisation
+    """
+    Execute minimax pour trouver le coup le plus pertinent
+    Simule les coups avec une alternance joueur adversaire pour trouver le coup donnant 
+    la plus grande chance de victoire 
+    
+    Variable:
+        matrice : (list) : Grille 2D du puissance 4 
+        depth : (int) : Profondeur (nombre de coup calculé à l'avance )
+        alpha: (float) : Valeur du meilleur score de l'algo
+        beta: (float) : Valeur du pire score de l'adversaire
+        maximizing: (bool) : True si l'algo doit maximiser le score, False dans le cas contraire
+        joueur : (int) : identifiant du joueur donc le tour est simulé 
+
+    Returns: 
+        best_col: (int où None): indice de la meilleur colonne a jouer (none si fin du jeu)
+        score: (float) : score de l'évaluation  de la position atteinte 
+    """
+    
     valid = coups_valides(matrice)
     terminal = verif_gagnant(matrice, 2) or verif_gagnant(matrice, 1) or len(valid) == 0 or depth == 0
 
