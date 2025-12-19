c7878a4427600a09dab8a5ddbf28595099b3e21e
ErwannX99 <erwannlesellier@hotmail.com>
2025-12-19 15:52:15 +0100

    opti de connnected.wav

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 # Importation des bibiloithèques
-from pyniryo import *       
-import time
-import random               
+from pyniryo import *              
 import serial
 
-from logique_jeu import gravite_piece, verif_gagnant, affichage        # Importation des fonctions cree sur les autres codes
+from logique_jeu import gravite_piece, verif_gagnant, affichage        # Importation des fonctions créer sur les autres codes
 from robot import port_serie, robot, pick_robot_piece
 
 port_serie = serial.Serial("COM3", baudrate=9600, timeout=2) # Ouverture de la liaison série
 
-# Creation de la matrice pour visualiser le jeu
+# Création de la matrice pour visualiser le jeu
 matrice = [[0 for _ in range(7)] for _ in range(6)]                
 
 joueur = 1
@@ -34,43 +32,39 @@ while True:
         while True:
             donnee_cm = port_serie.readline()                        # Lecture de la distance envoyer sur le port série
             try:
-                distance = int(donnee_cm.decode("utf-8").strip())    #Retire le prefix utf-8 et le transforme en int pour une lecture correcte
-                if not distance:                                     # Si aucune donné n'est reçue, recommence la boucle
+                distance = int(donnee_cm.decode("utf-8").strip())    #Retire le préfix utf-8 et le transforme en int pour une lecture correcte
+                if not distance:                                     # Si aucune donnée n'est reçue, recommence la boucle
                     continue
             except ValueError:
                 continue
 
-            if distance == 3:                       # Verifie la distance pour chaque colonne     
-                colonne = 0                         # Numero de la colonne 
-                robot.play_sound('connected.wav')   # Joue un son de confirmation
+            if distance == 3:                       # Vérifie la distance pour chaque colonne     
+                colonne = 0                         # Numéro de la colonne 
             elif distance == 7:
                 colonne = 1
-                robot.play_sound('connected.wav')
             elif distance == 11:
                 colonne = 2
-                robot.play_sound('connected.wav')
             elif distance == 14:
                 colonne = 3
-                robot.play_sound('connected.wav')
             elif distance == 18:
                 colonne = 4
-                robot.play_sound('connected.wav')
             elif distance == 22:
                 colonne = 5
-                robot.play_sound('connected.wav')
             elif distance == 26:
                 colonne = 6
-                robot.play_sound('connected.wav')
             else:
                 continue
             
-            # Verifie si le entrée colonne n'est pas hors limite    
+            if colonne <= 6 :
+                robot.play_sound('connected.wav')
+
+            # Vérifie si le entrée colonne n'est pas hors limite    
             if not (0 <= colonne < 7):              
                 print("Colonne hors limite.")
                 robot.play_sound('error.wav')
                 continue
 
-            # Verifie si la colonne est pleine
+            # Vérifie si la colonne est pleine
             if matrice[0][colonne] != 0:           
                 print("Colonne pleine.")
                 robot.play_sound('error.wav')
@@ -85,8 +79,8 @@ while True:
         colonne = pick_robot_piece(matrice, mode_robot)
         gravite_piece(matrice, colonne, joueur)
 
-# Verifie si la matrice est une matrice gagnante
-    if verif_gagnant(matrice, joueur):              #Selectionne la matrice a regarder et le joueur à verifier
+# Vérifie si la matrice est une matrice gagnante
+    if verif_gagnant(matrice, joueur):              # Sélectionne la matrice a regardé et le joueur à verifier
         affichage(matrice)                          # Affiche la matrice finale
         print(f"Le joueur {joueur} gagne !")        
         if joueur == 1:                             # Joue un son différent selon le gagnant
@@ -96,7 +90,7 @@ while True:
         robot.release_with_tool()
         break
 
-# Verifie si la matrice est pleine
+# Vérifie si la matrice est pleine
     if all(matrice[0][i] != 0 for i in range(7)):       
         affichage(matrice)                              
         print("Égalité, la grille est pleine.")         
